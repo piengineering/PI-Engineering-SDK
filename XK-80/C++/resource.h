3ac8c02f1cfd0dae0f1958e216df64b888a55781
P.I. Engineering <33100204+pattixkeys@users.noreply.github.com>
2022-09-27 13:25:48 -0400

    Add files via upload

---
+++ XK-80/C++/resource.h
@@ -0,0 +1,107 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PIHCallbackDemo.rc
+//
+#define IDD_MAIN                        101
+#define ID_EVENTS                       1000
+#define ID_EVENTMODE                    1001
+#define CHK_NONE                        1002
+#define CHK_NEW                         1003
+#define CHK_CHANGE                      1004
+#define IDHALT                          1005
+#define IDSTART                         1006
+#define IDC_LIST1                       1007
+#define IDC_UNITID                      1008
+#define IDC_EDIT1                       1009
+#define IDC_GREEN                       1010
+#define IDC_RED                         1011
+#define IDC_NOLED                       1012
+#define IDC_SETKEY                      1013
+#define IDC_SETKEY2                     1014
+#define IDC_CHECKKEY                    1014
+#define IDC_PASSFAIL                    1015
+#define IDC_WARNING                     1016
+#define IDC_TOREFLECT                   1016
+#define IDC_Multimedia                  1016
+#define IDC_BUTTON1                     1017
+#define IDC_CLEAR                       1017
+#define IDC_SaveBL                      1018
+#define IDC_TOPID5                      1018
+#define IDC_TOPID1                      1018
+#define IDC_BL2                         1019
+#define IDC_MyComputer                  1019
+#define IDC_TOSPLAT                     1020
+#define IDC_TOPID2                      1020
+#define IDC_TIMESTAMP                   1021
+#define IDC_KEYREFLECT                  1022
+#define IDC_JOYREFLECT                  1023
+#define IDC_MOUSEREFLECT                1024
+#define IDC_TIMESTAMP2                  1024
+#define IDC_MOUSEREFLECT2               1025
+#define IDC_DESCRIPTOR                  1025
+#define IDC_GENERATE                    1026
+#define IDC_MOUSEREFLECT3               1027
+#define IDC_MOUSEONOFF                  1028
+#define IDC_Version                     1028
+#define IDC_CHKGREENLED                 1029
+#define IDC_TOPID3                      1029
+#define IDC_CHKREDLED                   1030
+#define IDC_TOPID4                      1030
+#define IDC_CHKFGREENLED                1031
+#define IDC_CUSTOM                      1031
+#define IDC_CHKFREDLED                  1032
+#define IDC_NoDiodes                    1033
+#define IDC_NoDiodes2                   1034
+#define IDC_Diodes                      1034
+#define IDC_KEYSTATES                   1034
+#define IDC_KEYSTATES2                  1035
+#define IDC_KEYSTATES3                  1036
+#define IDC_UNITID3                     1037
+#define IDC_CHECK1                      1038
+#define IDC_CHECK2                      1039
+#define IDC_CHECK3                      1040
+#define IDC_EDIT2                       1040
+#define IDC_CHECK4                      1041
+#define IDC_JoyX                        1041
+#define IDC_JoyY                        1042
+#define IDC_JoyZ                        1043
+#define IDC_JoyZr                       1044
+#define IDC_JoySlider                   1045
+#define IDC_JoyGame1                    1046
+#define IDC_JoyGame2                    1047
+#define IDC_JoyGame3                    1048
+#define IDC_JoyGame4                    1049
+#define IDC_JoyHat                      1050
+#define IDC_TxtVersion                  1051
+#define IDC_MouseButtons                1052
+#define IDC_MouseX                      1053
+#define IDC_MouseY                      1054
+#define IDC_MouseWheel                  1055
+#define IDC_TxtMultiHi                  1056
+#define IDC_TxtMultiLo                  1057
+#define IDC_CALLBACK                    1058
+#define IDC_LblVersion                  1059
+#define IDC_TXTBL                       1060
+#define IDC_CHKBLONOFF                  1061
+#define IDC_CHKBLFLASH                  1062
+#define IDC_CHKBANK1                    1063
+#define IDC_CHKBANK2                    1064
+#define IDC_BLToggle                    1065
+#define IDC_BLIntensity                 1066
+#define IDC_BLSetInt                    1067
+#define IDC_BLIncInt                    1067
+#define IDC_SAVEBACKLIGHTS              1069
+#define IDC_BUTTON2                     1070
+#define IDC_FREQ                        1070
+#define IDC_EDIT3                       1071
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1072
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
