136327fcdb5e660c09e76791702f9726d1df9818
P.I. Engineering <33100204+pattixkeys@users.noreply.github.com>
2024-09-25 08:36:57 -0400

    Add files via upload

--- XBM-96 Module/VB/PIEHidDotNet Sample/Form1.Designer.vb
+++ XBM-96 Module/VB/PIEHidDotNet Sample/Form1.Designer.vb
@@ -2062,7 +2062,7 @@ Partial Class Form1
         Me.Controls.Add(Me.BtnEnumerate)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
-        Me.Text = " VB Sample for X-blox XBM-19 Module/XBE-96"
+        Me.Text = " VB Sample for X-keys XBM-96 Module/XBE-96"
         Me.groupBox3.ResumeLayout(False)
         Me.groupBox3.PerformLayout()
         Me.groupBox2.ResumeLayout(False)
